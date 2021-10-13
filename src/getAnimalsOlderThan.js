0f02b806815fabc3f84144e59aa3098c1f0c6457
Julia Barcelos <julaai@gmail.com>
2021-10-13 18:18:22 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+
+// Ao passar o nome de uma espécie e uma idade, testa se todos os animais desta espécie possuem a idade mínima especificada.
+// Retorna booleano.
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  // cria a variável e armazena todos os dados de cada animal daquela espécie.
+  const animalsData = species.find((specie) => specie.name === animal).residents;
+  return animalsData.every((animalResident) => animalResident.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -7,6 +7,4 @@ function getSpeciesByIds(...ids) {
   return ids;
 }
 
-getSpeciesByIds('e8481c1d-42ea-4610-8e11-1752cfc05a46, 0938aa23-f153-4937-9f88-4858b24d6bce');
-
 module.exports = getSpeciesByIds;
