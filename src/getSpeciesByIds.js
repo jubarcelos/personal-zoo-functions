98d06eac4688d5ed3e5b4ab0c1726eb57c763d81
Julia Barcelos <julaai@gmail.com>
2021-10-13 14:20:14 -0300

    subindo o PR

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+// function getSpeciesByIds(ids) {
+//   if (ids.length === 0) {
+//     console.log(ids);
+//     return data.species.filter((value) => !ids.includes(value.id));
+//   }
+//   console.log(data.species.filter((value) => ids.includes(value.id)));
+//   return data.species.filter((value) => ids.includes(value.id));
+// }
+// getSpeciesByIds();
+
+// module.exports = getSpeciesByIds;
+
+function filterIds(ids) {
+  return data.species.filter((value) => ids.includes(value.id));
 }
 
-module.exports = getSpeciesByIds;
+function getSpeciesByIds(...ids) {
+  if (ids.length === 0) return ids;
+  if (ids.length === 1) {
+    return [data.species.find((specie) => ids.includes(specie.id))];
+  }
+
+  if (ids.length > 1) return filterIds(ids);
+}
