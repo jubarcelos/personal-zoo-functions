0906318f447bc07f0fb5948ce31bbf6f6402fd71
Julia Barcelos <julaai@gmail.com>
2021-10-13 15:05:13 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,26 +1,12 @@
 const data = require('../data/zoo_data');
 
-// function getSpeciesByIds(ids) {
-//   if (ids.length === 0) {
-//     console.log(ids);
-//     return data.species.filter((value) => !ids.includes(value.id));
-//   }
-//   console.log(data.species.filter((value) => ids.includes(value.id)));
-//   return data.species.filter((value) => ids.includes(value.id));
-// }
-// getSpeciesByIds();
-
-// module.exports = getSpeciesByIds;
-
-function filterIds(ids) {
-  return data.species.filter((value) => ids.includes(value.id));
-}
-
 function getSpeciesByIds(...ids) {
-  if (ids.length === 0) return ids;
-  if (ids.length === 1) {
-    return [data.species.find((specie) => ids.includes(specie.id))];
+  if (ids.length !== undefined) {
+    return data.species.filter((value) => ids.includes(value.id));
   }
-
-  if (ids.length > 1) return filterIds(ids);
+  return ids;
 }
+
+getSpeciesByIds('e8481c1d-42ea-4610-8e11-1752cfc05a46, 0938aa23-f153-4937-9f88-4858b24d6bce');
+
+module.exports = getSpeciesByIds;
