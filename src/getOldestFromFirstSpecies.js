1e1e408dd10a3c3bb4368f9365ccfb8aa6b8b26d
Julia Barcelos <julaai@gmail.com>
2021-10-15 23:50:37 -0300

    requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,20 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+const { employees } = data;
+// Passado o id de um funcionário, encontra a primeira espécie de animal gerenciado pelo funcionário, e retorna um array com nome, sexo e idade do animal mais velho dessa espécie.
 
+function getOldestFromFirstSpecies(id) {
+  const employee = employees.find((employ) => employ.id === id);
+  const specie = employee.responsibleFor[0];
+  const specieDetails = species.find((animal) => specie === animal.id);
+
+  const { residents } = specieDetails;
+  const speciesAge = residents.map((resident) => resident.age);
+  const oldAge = Math.max(...speciesAge);
+  const oldAnimal = residents.find((animal) => animal.age === oldAge);
+
+  const { name, sex, age } = oldAnimal;
+  return [name, sex, age];
+}
 module.exports = getOldestFromFirstSpecies;
